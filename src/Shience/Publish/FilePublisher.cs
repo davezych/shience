632329efd394251d5cb98be57b11bbe9000937bd
David Zych <dave@davidzych.com>
2015-03-23 14:23:00 -0700

    Pass in file path to constructor

--- src/Shience/Publish/FilePublisher.cs
+++ src/Shience/Publish/FilePublisher.cs
@@ -5,11 +5,16 @@ namespace Shience.Publish
 {
     public class FilePublisher<TResult> : IPublisher<TResult>
     {
-        private const string PublishFilePath = @"D:\scienceResults.txt";
+        private readonly string _filePath;
+
+        public FilePublisher(string path)
+        {
+            _filePath = path;
+        } 
 
         public void Publish(ExperimentResult<TResult> result)
         {
-            using (var sw = new StreamWriter(PublishFilePath, true))
+            using (var sw = new StreamWriter(_filePath, true))
             {
                 //TODO: Need to add exception (if any) to publishing
                 sw.WriteLine(result.TestName + "|" + result.ControlResult.Result + "|" + result.ControlResult.RunTime + "|" + result.CandidateResult.Result + "|" + result.CandidateResult.RunTime + "|" + result.Matched);
--- src/Shience/Shience.cs
+++ src/Shience/Shience.cs
@@ -7,13 +7,13 @@ namespace Shience
     {
         public static Science<TResult> New<TResult>(string name)
         {
-            var publisher = new FilePublisher<TResult>();
+            var publisher = new FilePublisher<TResult>(@"D:\shienceResults.txt");
             return new Science<TResult>(name, publisher);
         }
 
         public static Science<TResult> New<TResult>(string name, IComparer<TResult> comparer)
         {
-            var publisher = new FilePublisher<TResult>();
+            var publisher = new FilePublisher<TResult>(@"D:\shienceResults.txt");
             return new Science<TResult>(name, publisher, comparer);
         }
     }
