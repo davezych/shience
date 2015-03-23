9a97fc2876c77a90d9f0c8aa9be543702c025398
David Zych <dave@davidzych.com>
2015-03-23 14:15:19 -0700

    Added static factory-ish class for Science

---
+++ src/Shience/Shience.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Shience
+{
+    public static class Shience
+    {
+        public static Science<TResult> New<TResult>(string name)
+        {
+            return new Science<TResult>(name);
+        }
+
+        public static Science<TResult> New<TResult>(string name, IComparer<TResult> comparer)
+        {
+            return new Science<TResult>(name, comparer);
+        }
+    }
+}
