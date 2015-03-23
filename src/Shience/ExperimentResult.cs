1db7e026a530187839647183400ad4789a2827e0
David Zych <dave@davidzych.com>
2015-03-23 13:45:23 -0700

    Added ability to use custom comparer

--- src/Shience/ExperimentResult.cs
+++ src/Shience/ExperimentResult.cs
@@ -1,14 +1,27 @@
-﻿namespace Shience
+﻿using System.Collections.Generic;
+
+namespace Shience
 {
     public class ExperimentResult<TResult>
     {
         public string TestName { get; set; }
         public TestResult<TResult> ControlResult { get; set; }
         public TestResult<TResult> CandidateResult { get; set; }
+        private readonly IComparer<TResult> _comparer;
+
+        public ExperimentResult()
+        {
+            _comparer = Comparer<TResult>.Default;
+        }
+        
+        public ExperimentResult(IComparer<TResult> comparer)
+        {
+            _comparer = comparer;
+        }
 
         public bool Matched
         {
-            get { return ControlResult.Result.Equals(CandidateResult.Result); }
+            get { return _comparer.Compare(ControlResult.Result, CandidateResult.Result) == 0; }
         }
     }
 }
