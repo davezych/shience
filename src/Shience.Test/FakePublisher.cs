c59388605b64d769ff7ec61e9d2aa59de3f4f4d1
Dave Zych <dave@davidzych.com>
2015-03-23 15:09:11 -0700

    Added tests

---
+++ src/Shience.Test/FakePublisher.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Shience.Publish;
+using Shience.Result;
+
+namespace Shience.Test
+{
+    class FakePublisher<TResult> : IPublisher<TResult>
+    {
+        public void Publish(ExperimentResult<TResult> result)
+        {
+            Debug.WriteLine(result.TestName + "|" + result.Matched);
+        }
+    }
+}
---
+++ src/Shience.Test/ScienceTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shience.Publish;
+
+namespace Shience.Test
+{
+    [TestClass]
+    public class ScienceTests
+    {
+        [TestInitialize]
+        public void ScienceTestsInitialize()
+        {
+            Shience.SetPublisher(typeof(FakePublisher<>));
+        }
+
+        [TestMethod]
+        public void DefaultComparerReturnsTrueWithSameResultOnPrimitives()
+        {
+            var science = Shience.New<bool>("Test");
+
+            var result = science.Test(control: (() => { return true; }), candidate: (() => { return true; }));
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void DefaultComparerReturnsFalseWithDifferentResultOnPrimitives()
+        {
+            var science = Shience.New<bool>("Test");
+
+            var result = science.Test(control: (() => { return true; }), candidate: (() => { return false; }));
+
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void DefaultComparerReturnsTrueWithSameResultOnObject()
+        {
+            var science = Shience.New<TestHelper>("Test");
+
+            var result = science.Test(control: (() => { return new TestHelper {Number = 1}; }),
+                candidate: (() => { return new TestHelper {Number = 1}; }));
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void DefaultComparerReturnsFalseWithDifferentResultOnObject()
+        {
+            var science = Shience.New<TestHelper>("Test");
+
+            var result = science.Test(control: (() => { return new TestHelper {Number = 1}; }),
+                candidate: (() => { return new TestHelper {Number = 2}; }));
+
+            Assert.AreEqual(true, result);
+        }
+
+        private class TestHelper
+        {
+            public int Number { get; set; }
+        }
+    }
+}
