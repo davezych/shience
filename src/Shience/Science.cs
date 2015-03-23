e8ec248faf31418bc6777d157bc3fbe40027c87a
David Zych <dave@davidzych.com>
2015-03-23 13:36:29 -0700

    Added testname field

--- src/Shience/Science.cs
+++ src/Shience/Science.cs
@@ -4,6 +4,13 @@ namespace Shience
 {
     public class Science<TResult>
     {
+        private readonly string _testName;
+
+        internal Science(string testName)
+        {
+            _testName = testName;
+        }
+
         public TResult Test(Func<TResult> control, Func<TResult> candidate)
         {
             var controlResult = control();
