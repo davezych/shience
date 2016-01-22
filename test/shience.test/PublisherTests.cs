b3da945007ce4b50d13b7c4ef303b98ddcce3b52
Dave Zych <david.zych@dptechnology.com>
2016-01-22 09:02:43 -0800

    Added first publisher test

---
+++ test/shience.test/PublisherTests.cs
@@ -0,0 +1,18 @@
+﻿using Shience.Test.Fakes;
+using Xunit;
+
+namespace Shience.Test
+{
+    public class PublisherTests
+    {
+        [Fact]
+        public void ShienceCanInstantiateWithPublisher()
+        {
+            Sciencer.SetPublisher(new FakePublisher());
+
+            var science = Sciencer.New<bool>("ShienceCanInstantiateFilePublisher");
+
+            Assert.NotNull(science);
+        }
+    }
+}
