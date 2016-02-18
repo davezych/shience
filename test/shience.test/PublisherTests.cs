d99cf509da1f099f7a848b06b5ffc39a82193c23
Dave Zych <david.zych@dptechnology.com>
2016-02-18 14:41:42 -0800

    Added publisher tests

--- test/shience.test/PublisherTests.cs
+++ test/shience.test/PublisherTests.cs
@@ -5,11 +5,48 @@ namespace Shience.Test
     public sealed class PublisherTests
     {
         [Fact]
-        public void ShienceCanInstantiateWithPublisher()
+        public void PublishIsCalledAfterExecute()
         {
-            var science = Science.New<bool>("ShienceCanInstantiateFilePublisher");
-            
-            Assert.NotNull(science);
+            var experiment = Science.New<bool>("PublishIsCalledAfterExecute");
+
+            var matched = false;
+
+            experiment.Test<bool>(control: () => true, candidate: () => true)
+                .PublishTo((e) => matched = e.Matched)
+                .Execute();
+
+            Assert.True(matched);
+        }
+
+        [Fact]
+        public void AllPublishersAreCalledAfterExecute()
+        {
+            var experiment = Science.New<bool>("AllPublishersAreCalledAfterExecute");
+
+            var matchedOne = false;
+            var matchedTwo = false;
+            var matchedThree = false;
+
+            experiment.Test<bool>(control: () => true, candidate: () => true)
+                .PublishTo((e) => matchedOne = e.Matched)
+                .PublishTo((e) => matchedTwo = e.Matched)
+                .PublishTo((e) => matchedThree = e.Matched)
+                .Execute();
+
+            Assert.True(matchedOne);
+            Assert.True(matchedTwo);
+            Assert.True(matchedThree);
+        }
+
+        [Fact]
+        public void ExecuteRunsWithNoPublisher()
+        {
+            var experiment = Science.New<bool>("PublishIsCalledAfterExecute");
+
+            var result = experiment.Test<bool>(control: () => true, candidate: () => true)
+                .Execute();
+
+            Assert.True(result);
         }
     }
 }
